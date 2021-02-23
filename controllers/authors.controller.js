ac3d753d5de51172f93bf5e5af29dbc1e285efa4
nngocjade <nngocjade@gmail.com>
2021-02-23 18:13:42 +0700

    created a new author

--- controllers/authors.controller.js
+++ controllers/authors.controller.js
@@ -1,6 +1,22 @@
-const createAuthor = (req, res) => {
-  const name = req.body.name;
-  res.send(`Hello ${name}!!!`);
+const Author = require("../models/author");
+
+const createAuthor = async (req, res) => {
+  try {
+    const author = new Author({ name: req.body.name });
+
+    await author.save();
+
+    res.status(201).json({
+      success: true,
+      data: author,
+      message: `Author ${author.name} created!`,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      error: err.message,
+    });
+  }
 };
 
 module.exports = {
